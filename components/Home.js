95c2ff18badee0595b5a08f75e94d94d3895a3b6
christychoi <christychoi.dev@gmail.com>
2022-07-28 17:21:45 +0900

    backend basic done frontend setup

---
+++ components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Filters from "./layout/Filters";
+import JobItem from "./job/JobItem";
+import Link from 'next/link';
+
+const Home = ({data}) => {
+
+  const {jobs, count, resPerPage} = data
+
+  return (
+    <>
+      <div className="container container-fluid">
+        <div className="row">
+          <div className="col-xl-3 col-lg-4">
+            <Filters />
+          </div>
+
+          <div className="col-xl-9 col-lg-8 content-left-offset">
+            <div className="my-5">
+              <h4 className="page-title">Latest Jobs</h4>
+              <Link href="/stats">
+                <button className="btn btn-secondary float-right stats_btn">
+                  Get Topic stats
+                </button>
+              </Link>
+              <div className="d-block">
+                <Link href="/search">Go to Search</Link>
+              </div>
+            </div>
+            {jobs && jobs.map((job) => <JobItem key={job.id} />)}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
